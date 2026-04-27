c643cf673cf9254c24cbad2da5cbf99397454282
leanhtubt <162678881+leanhtubt@users.noreply.github.com>
2026-04-27 17:25:48 +0000

    Fix: Force include headers to resolve YouSpeed forward declaration

---
+++ Tweaks/FixHeaders/FixForward.h
@@ -0,0 +1,7 @@
+#import <YouTubeHeader/YTVarispeedSwitchControllerImpl.h>
+#import <YouTubeHeader/YTVarispeedSwitchController.h>
+
+// Định nghĩa interface để sửa lỗi forward declaration
+@interface YTVarispeedSwitchControllerImpl : NSObject
+- (void)setValue:(id)value forKey:(NSString *)key;
+@end
