c93e09ae9f3aac68955399b39850e4f9f89c3fee
leanhtubt <162678881+leanhtubt@users.noreply.github.com>
2026-04-28 01:34:55 +0700

    Update YTVarispeedSwitchController.h

--- Tweaks/FixHeaders/YouTubeHeader/YTVarispeedSwitchController.h
+++ Tweaks/FixHeaders/YouTubeHeader/YTVarispeedSwitchController.h
@@ -1,1 +1,8 @@
-404: Not Found
+#import <Foundation/Foundation.h>
+
+@interface YTVarispeedSwitchControllerImpl : NSObject
+{
+    NSArray* _options;
+}
+@property(copy, nonatomic) NSArray *options;
+@end
